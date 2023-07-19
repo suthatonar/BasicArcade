ce7540d28dca1618a7ffd88c541de5fc4dfcd72a
suthatonar <139983715+suthatonar@users.noreply.github.com>
2023-07-19 22:06:49 +0700

    Delete Scripts directory

--- Scripts/Recipe.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RecipeData", menuName = "CreateRecipe/RecipeData", order = 1)]
-public class Recipe: ScriptableObject
-{
-    public List<item.itemType> type = new List<item.itemType>();
-}
