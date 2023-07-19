ea0ec39156d8d10700ce1446f1d275480438d152
suthatonar <139983715+suthatonar@users.noreply.github.com>
2023-07-19 22:27:06 +0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.PlayerSettings;
 
 public class Enemy : MonoBehaviour
 {
